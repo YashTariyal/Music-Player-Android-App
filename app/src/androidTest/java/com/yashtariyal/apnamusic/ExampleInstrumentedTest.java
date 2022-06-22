2f0639eac91d1e47ce5cbcd27964c54a94822fcb
Yash Tariyal <77398467+YashTariyal@users.noreply.github.com>
2022-06-22 17:13:15 +0530

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/yashtariyal/apnamusic/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/yashtariyal/apnamusic/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.harshsharma.apnamusic;
+package com.yashtariyal.apnamusic;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.harshsharma.apnamusic", appContext.getPackageName());
+        assertEquals("com.yashtariyal.apnamusic", appContext.getPackageName());
     }
 }
